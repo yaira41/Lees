da8a7f6e82c1c41c43e5473abb880dab768163ce
yairavivi4 <yavivi441@gmail.com>
2021-06-23 17:12:47 +0300

    start working - add few folders, employee class and enum

---
+++ LeesBirthClass/Enums/EmployeesTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeesBirthClass.Enums
+{
+    enum EmployeesTypes
+    {
+        Adminstrative,
+        Professional
+    }
+}
