d1cb0a46765f2836ab6f88cb1461519031c4f727
yairavivi4 <yavivi441@gmail.com>
2021-06-23 19:15:14 +0300

    add enum and proceeed working on employees

--- LeesBirthClass/Employees/Employee.cs
+++ LeesBirthClass/Employees/Employee.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LeesBirthClass.Enums;
 
 namespace LeesBirthClass.Employees
 {
     class Employee
     {
+        public Employee(string firstName, string lastName, List<Ranks> ranks)
+        {
+            Id = Guid.NewGuid().ToString();
+            FirstName = firstName;
+            LastName = lastName;
+            Ranks = ranks;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public string Id { get; private set; }
+
+        public List<Ranks> Ranks { get; set; }
+
+        public float Salary { get; set; }
+
+
     }
 }
